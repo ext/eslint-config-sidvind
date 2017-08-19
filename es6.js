fd335fe98b0d66f15ff914730c43c6dbeb86d013
David Sveningsson <ext@sidvind.com>
2017-08-19 17:56:53 +0200

    es6 rules

---
+++ es6.js
@@ -0,0 +1,13 @@
+module.exports = {
+	extends: [
+		"sidvind",
+	],
+
+	rules: {
+		"no-var": "error",
+		"prefer-const": "error",
+		"prefer-rest-params": "error",
+		"prefer-spread": "error",
+		"prefer-template": "error",
+	},
+};
